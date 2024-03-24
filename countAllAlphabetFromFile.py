03fd9846e6a0154b39925270f318442f598f9e9c
jaweed2010 <mdjaweed2010@gmail.com>
2024-03-25 02:27:50 +0530

    removed unused vars

--- countAllAlphabetFromFile.py
+++ countAllAlphabetFromFile.py
@@ -5,7 +5,6 @@ try:
 except IOError as e:
     print("Cannot open the source file: ", strerror(e.errno))
     exit(e.errno)
-    st=''
 
 dictn = {}
 
@@ -14,11 +13,11 @@ try:
     while ch != '':
         if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
             dictn[ch.lower()] = dictn.get(ch.lower(), 0) + 1
-        ch=src.read(1)
+        ch = src.read(1)
 except IOError as e:
     print("Cannot create the destination file: ", strerror(e.errno))
     exit(e.errno)
 
 print(dict, 'byte(s) succesfully written')
-print("sorted:",dict(sorted(dictn.items(), key=lambda item: item[1], reverse=True)))
+print("sorted:", dict(sorted(dictn.items(), key=lambda item: item[1], reverse=True)))
 src.close()
